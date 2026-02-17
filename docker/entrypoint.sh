96fd840710d58811af13f78cd75785a213bfe17e
jamie-at-bunny <jamie@bunny.net>
2026-02-17 09:36:53 +0000

    wait for mariadb

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,13 @@ touch .env
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+echo "Waiting for database..."
+until php artisan db:monitor --databases=mariadb > /dev/null 2>&1; do
+    sleep 1
+done
+echo "Database is ready."
+
 php artisan migrate --force
 
 exec supervisord -c /etc/supervisord.conf
